e5a6a93fd778fb483c60a02c89c69be3962a5b43
Henry Li <henry1943@163.com>
2016-11-30 15:28:43 +0800

    Add server.js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-(async () => {
-    console.log("Success");
-})();
+import httpServer from "./lib/http/server";
+
+const port = 8000;
+httpServer.listen(port, () => {
+    console.log(`Server is now listening at port ${port}...`);
+});
